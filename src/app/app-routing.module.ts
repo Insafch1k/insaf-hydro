9acf455404d1ab1dee0e1f25b7fc7163b4539eae
Insafch1k <ainsaf833@gmail.com>
2024-11-06 13:05:46 +0300

    начало

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './website/shared/pages/main/main.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    title: 'main',
+},
+{
+  path: 'about',
+  loadChildren: () =>
+      import('./website/modules/about/about.module').then(
+          (m) => m.AboutModule
+      ),
+  title: 'about',
+},
+{
+  path: '**',
+  component: MainComponent,
+  redirectTo: '',
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './website/shared/pages/main/main.component';
+import { HeaderComponent } from './website/shared/layout/header/header.component';
+import { AboutModule } from './website/modules/about/about.module';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AboutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
